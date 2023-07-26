e22393a6f7c341c25d0682a50c71399a0c01fe3f
Jcapuzzi <89081132+Jcapuzzi@users.noreply.github.com>
2023-07-26 20:16:21 -0300

    Update entrega 3.py

--- entrega 3.py
+++ entrega 3.py
@@ -1,11 +1,6 @@
-# Este es el DAG que orquesta el ETL de la tabla users
-
 import pandas as pd
 import os
-import json
-import logging
 import psycopg2
-import numpy
 import yfinance as yf
 from airflow import DAG
 from airflow.macros import ds_add
@@ -19,12 +14,43 @@ from psycopg2.extras import execute_values
 default_args = {
     'owner': 'Capuzzi',
     'depends_on_past': False,
-    'start_date': datetime(2023, 7, 18),
-    'retries': 1,
+    'email':['capuzzi022@gmail.com'],
+    'email_on_retry': True,
+    'email_on_failure': True,
+    'start_date': datetime(2023, 7, 27),
+    'retries': 2,
     'retry_delay': timedelta(minutes=5),
 }
-env = os.environ
-try:
+
+
+hmy = yf.Ticker('HMY')
+
+    
+def importar_data():
+
+ hist = hmy.history(period="1y")
+ hist['Date']=hist.index
+ hist=hist.reset_index(drop=True)
+ hist.to_csv("/opt/airflow/dags/hist.csv")
+ 
+def transformar():
+    
+ hist=pd.read_csv("opt/airfow/dags/hist.csv")
+ hist= hist.rename(columns={"Open":"Apertura","High":"Precio_alto","Low":"Precio_bajo","Close":"Cierre","Volume":"Volumen","Dividends":"Dividendos","Stock Splits":"Stock","Date":"Dia"})
+ hist= hist.drop(columns=["Dividendos","Stock","Volumen"])
+ hist.insert(
+ loc=0,
+ column= "Empresa",
+ value="Harmony Gold",
+ allow_duplicates=False)
+ print(hist)
+ Harmony_sin_duplicados= hist.drop_duplicates()#Elimino duplicados
+ print(Harmony_sin_duplicados) 
+ Harmony_sin_duplicados.to_csv("/opt/airflow/dags/Harmony_sin_duplicados.csv")
+ 
+def Cargar_data():
+ env = os.environ
+ try:
     conn =psycopg2.connect( host=env['REDSHIFT_HOST'],
                            dbname="data-engineer-database",
                            user= env['REDSHIFT_USER'],
@@ -32,51 +58,19 @@ try:
                            port="5439"
                           )
     print("me conecte")
-except Exception as e:
+ except Exception as e:
     print("odio esto")
-    print(e) 
-    cursor = conn.cursor()
-    cursor.execute(f"""
-    create table if not exists {"jcapuzzi_coderhouse"}.Harmony_sin_duplicados (
-    Empresa VARCHAR(30),
-    Apertura decimal(20,2),
-    Precio_alto decimal(20,2),
-    Precio_bajo decimal(20,2),
-    Cierre decimal(20,2),
-    Dia date distkey
-) sortkey(Dia);
-""")
-    conn.commit()
-    cursor.close()
-    print("Table created!")
-
-
-   
-    
-def importar_data():
-  hmy = yf.Ticker('HMY')
-  hist = hmy.history(period="5y")
-  hist['Date']=hist.index
-  hist=hist.reset_index(drop=True)
-  hist= hist.rename(columns={"Open":"Apertura","High":"Precio_alto","Low":"Precio_bajo","Close":"Cierre","Volume":"Volumen","Dividends":"Dividendos","Stock Splits":"Stock","Date":"Dia"})
-  hist= hist.drop(columns=["Dividendos","Stock","Volumen"])
-  hist.insert(
- loc=0,
- column= "Empresa",
- value="Harmony Gold",
- allow_duplicates=False)
-  print(hist)
-  Harmony_sin_duplicados= hist.drop_duplicates()#Elimino duplicados
-  print(Harmony_sin_duplicados) 
-
-  cursor= conn.cursor ()
-  table_name="harmony_sin_duplicados"
-  columns=["Empresa","Apertura","precio_alto","Precio_bajo","Cierre","Dia"]
-  values=[tuple(x) for x in Harmony_sin_duplicados.to_numpy()]
-  insert_sql= f"INSERT INTO {table_name} ({','.join(columns)}) VALUES %s"
-  cursor.execute("BEGIN")
-  execute_values(cursor, insert_sql, values)
-  cursor.execute("COMMIT")
+    print(e)   
+ Harmony_sin_duplicados=pd.read_csv("/opt/airflow/dags/Harmony_sin_duplicados.csv")
+ cursor= conn.cursor ()
+ table_name="Harmonu_sin_duplicados"
+ columns=["Empresa","Apertura","precio_alto","Precio_bajo","Cierre","Dia"]
+ values=[tuple(x) for x in Harmony_sin_duplicados.to_numpy()]
+ insert_sql= f"INSERT INTO {table_name} ({','.join(columns)}) VALUES %s"
+ cursor.execute("BEGIN")
+ execute_values(cursor, insert_sql, values)
+ cursor.execute("COMMIT")  
+ 
 
 with DAG (
     'First',
@@ -84,8 +78,12 @@ with DAG (
     
 ) as dag:
     
- Tarea_1=PythonOperator(task_id='importar_data',python_callable=importar_data)
+  Tarea_1=PythonOperator(task_id='importar_data',python_callable=importar_data)
+  Tarea_2=PythonOperator(task_id='transformar',python_callable=transformar)
+  Tarea_3=PythonOperator(task_id='Cargar_data',python_callable=Cargar_data)
+
+
+Tarea_1>>Tarea_2>>Tarea_3
 
 
 
-Tarea_1
